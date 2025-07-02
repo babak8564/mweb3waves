bdc62e5ccde5a0c4a01d44697b3bc2643cd9627d
Babak Darabi <58424847+babak8564@users.noreply.github.com>
2025-07-02 18:55:48 +0330

    Update server.js
    for vercel

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-	res.sendFile('index.html', { root : __dirname});
+	res.sendFile('index.html', { root : path.join(__dirname, 'public')});
 });
 
 let port = process.env.PORT || 5000;
