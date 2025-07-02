50fa833afc25dcca481299a6a0509328f1a5650d
babak8564 <darabi.babak@gmail.com>
2025-07-02 18:38:09 +0330

    Add .gitattributes to enforce LF line endings

--- src/components/app.js
+++ src/components/app.js
@@ -1,21 +1,22 @@
-import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client';
 import React from 'react';
 
 class App extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {};
     }
     render() {
         return (
             <div className="container">
-    		    <input className="btn btn-primary" type="submit" value="Alert" onClick={() => {alert("Alert button onClick");}}/>
-    	    </div>
-        )
+                <input className="btn btn-primary" type="submit" value="Alert" onClick={() => { alert("Alert button onClick"); }} />
+            </div>
+        );
     }
 }
 
 const app = document.getElementById('app');
-if(app){
-    ReactDOM.render(<App/>, app);
+if (app) {
+    const root = createRoot(app);
+    root.render(<App />);
 }
